4d7951431d444e5829edfe93048514aa53b19057
Marnen Laibow-Koser <marnen@marnen.org>
2013-04-12 13:47:25 -0400

    Remove function wrapper. Apparently Greasemonkey/Tampermonkey already do this.
    This reverts commit 59ce2d297146609f1fb84c3e010ca41157319ea9.

--- better-fetlife-userscript.user.js
+++ better-fetlife-userscript.user.js
@@ -16,92 +16,90 @@
 // @grant          GM_log
 // ==/UserScript==
 
-(function () {
-    var FL_BETTER = {};
-    FL_BETTER.CONFIG = {
-        'debug': false, // switch to true to debug.
+FL_BETTER = {};
+FL_BETTER.CONFIG = {
+    'debug': false, // switch to true to debug.
+};
+
+// Utility debugging function.
+FL_BETTER.log = function (msg) {
+    if (!FL_BETTER.CONFIG.debug) { return; }
+    GM_log('BETTER FETLIFE: ' + msg);
+};
+
+// Initializations.
+FL_BETTER.init = function () {
+    FL_BETTER.main();
+};
+window.addEventListener('DOMContentLoaded', FL_BETTER.init);
+
+// This is the main() function, executed on page load.
+// TODO: Mark up events on user pages and users on event pages.
+FL_BETTER.main = function () {
+    // Determine what type of page and what its ID number is.
+    var m = window.location.pathname.match(/^\/(user|event)s\/(\d+)$/);
+    FL_BETTER.log('Loaded page for ' + m[1] + ' number ' + m[2] + '.');
+    switch (m[1]) {
+        // If on a user profile page,
+        case 'user':
+            FL_BETTER.processUser();
+        break;
+
+        // If on an event page,
+        case 'event':
+            FL_BETTER.processEvent();
+        break;
+    }
+};
+
+FL_BETTER.processEvent = function () {
+    var parseDateTime = function (metaDateTimeElement) {
+        return metaDateTimeElement.attr('content').replace(/Z$/, '');
     };
 
-    // Utility debugging function.
-    FL_BETTER.log = function (msg) {
-        if (!FL_BETTER.CONFIG.debug) { return; }
-        GM_log('BETTER FETLIFE: ' + msg);
+    var dateTimeMarkup = function (dateTimeString) {
+        return $('<abbr class="value"></abbr>').attr('title', dateTimeString);
     };
 
-    // Initializations.
-    FL_BETTER.init = function () {
-        FL_BETTER.main();
-    };
-    window.addEventListener('DOMContentLoaded', FL_BETTER.init);
-
-    // This is the main() function, executed on page load.
-    // TODO: Mark up events on user pages and users on event pages.
-    FL_BETTER.main = function () {
-        // Determine what type of page and what its ID number is.
-        var m = window.location.pathname.match(/^\/(user|event)s\/(\d+)$/);
-        FL_BETTER.log('Loaded page for ' + m[1] + ' number ' + m[2] + '.');
-        switch (m[1]) {
-            // If on a user profile page,
-            case 'user':
-                FL_BETTER.processUser();
-            break;
-
-            // If on an event page,
-            case 'event':
-                FL_BETTER.processEvent();
-            break;
-        }
+    var addTimeMarkup = function (startOrEnd) {
+        var timeElement = $('[itemprop=' + startOrEnd + 'Date]');
+        var dateTimeString = timeElement[0] ? parseDateTime(timeElement) : null;
+        var container = $('<span></span>').addClass('dt' + startOrEnd);
+        container.append(dateTimeMarkup(dateTimeString));
+        timeElement.after(container);
     };
 
-    FL_BETTER.processEvent = function () {
-        var parseDateTime = function (metaDateTimeElement) {
-            return metaDateTimeElement.attr('content').replace(/Z$/, '');
-        };
+    $("[itemtype='http://schema.org/Event']").addClass('vevent');
+    $('h1[itemprop=name]').addClass('summary');
+    $('[itemprop=description]').addClass('description');
 
-        var dateTimeMarkup = function (dateTimeString) {
-            return $('<abbr class="value"></abbr>').attr('title', dateTimeString);
-        };
+    // TODO: Parse venues out of location data and mark them up using hCards
+    //       See: http://microformats.org/wiki/hcalendar-brainstorming#hCard_locations
+    $($('[itemprop=location]').parents().children('span')[1]).addClass('location');
 
-        var addTimeMarkup = function (startOrEnd) {
-            var timeElement = $('[itemprop=' + startOrEnd + 'Date]');
-            var dateTimeString = timeElement[0] ? parseDateTime(timeElement) : null;
-            var container = $('<span></span>').addClass('dt' + startOrEnd);
-            container.append(dateTimeMarkup(dateTimeString));
-            timeElement.after(container);
-        };
+    addTimeMarkup('start');
+    addTimeMarkup('end');
 
-        $("[itemtype='http://schema.org/Event']").addClass('vevent');
-        $('h1[itemprop=name]').addClass('summary');
-        $('[itemprop=description]').addClass('description');
+    // Write out URL.
+    $('.vevent .description').append('<a style="display: none;" class="url" href="' + window.location.href + '">Make FetLife Better.</a>');
+};
 
-        // TODO: Parse venues out of location data and mark them up using hCards
-        //       See: http://microformats.org/wiki/hcalendar-brainstorming#hCard_locations
-        $($('[itemprop=location]').parents().children('span')[1]).addClass('location');
+FL_BETTER.processUser = function () {
+    // Add hCard classes for markup that's done right.
+    $('#profile').addClass('vcard');
+    $('#profile .pan').addClass('photo');
+    $('h2.bottom + p').addClass('adr');
+    $($('h2.bottom + p a[href^="/cities"]')).addClass('locality');
+    $($('h2.bottom + p a[href^="/administrative_areas"]')).addClass('region');
+    $($('h2.bottom + p a[href^="/countries"]')).addClass('country-name');
 
-        addTimeMarkup('start');
-        addTimeMarkup('end');
+    // Isolate name and add microformat markup.
+    $($('h2.bottom').contents()[0]).wrap('<span class="fn" />');
 
-        // Write out URL.
-        $('.vevent .description').append('<a style="display: none;" class="url" href="' + window.location.href + '">Make FetLife Better.</a>');
-    };
+    // Isolate "About me" content and add microformat markup.
+    $($('h3.bottom::first-child').nextUntil('h3.bottom')).wrapAll('<div class="note" />')
 
-    FL_BETTER.processUser = function () {
-        // Add hCard classes for markup that's done right.
-        $('#profile').addClass('vcard');
-        $('#profile .pan').addClass('photo');
-        $('h2.bottom + p').addClass('adr');
-        $($('h2.bottom + p a[href^="/cities"]')).addClass('locality');
-        $($('h2.bottom + p a[href^="/administrative_areas"]')).addClass('region');
-        $($('h2.bottom + p a[href^="/countries"]')).addClass('country-name');
+    // Write out URL.
+    $('#profile').append('<a style="display: none;" class="url" href="' + window.location.href + '">Make FetLife Better.</a>');
 
-        // Isolate name and add microformat markup.
-        $($('h2.bottom').contents()[0]).wrap('<span class="fn" />');
-
-        // Isolate "About me" content and add microformat markup.
-        $($('h3.bottom::first-child').nextUntil('h3.bottom')).wrapAll('<div class="note" />')
-
-        // Write out URL.
-        $('#profile').append('<a style="display: none;" class="url" href="' + window.location.href + '">Make FetLife Better.</a>');
-
-    };
-})();
+};
